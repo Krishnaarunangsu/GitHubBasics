9a96a4fae95048f48a7333c07a453681a0fc0678
Krishnaarunangsu <arunangsutech@gmail.com>
2019-03-24 20:18:32 +0530

    first commit

---
+++ src/main/java/com/git/tutorial/Student.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.git.tutorial;
+
+import java.io.Serializable;
+
+/**
+ * @author arunangsu.sahu
+ *
+ */
+public class Student implements Serializable {
+
+	private String studentId;
+	private String studentName;
+	/**
+	 * 
+	 */
+	public Student() {
+		// No-argument Constructor
+	}
+	/**
+	 * @return the studentId
+	 */
+	public String getStudentId() {
+		return studentId;
+	}
+	/**
+	 * @param studentId the studentId to set
+	 */
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	/**
+	 * @return the studentName
+	 */
+	public String getStudentName() {
+		return studentName;
+	}
+	/**
+	 * @param studentName the studentName to set
+	 */
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+	
+
+}
